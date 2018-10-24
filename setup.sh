0afc792cc78a406a0163b41affacaa5f9335c6c1
Justin Shepherd <jshepher@rackspace.com>
2018-10-24 11:36:25 -0500

    adding packer install to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-apt-get install -y lxc apt-cacher-ng
+apt-get install -y lxc apt-cacher-ng unzip
+
+wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_amd64.zip
+unzip -d /usr/local/sbin packer_1.3.1_linux_amd64.zip
+
+packer -version
