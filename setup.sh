a007dd313a14ccdd032d8120859af9ec4f7c1a97
Justin Shepherd <jshepher@rackspace.com>
2018-10-24 11:39:08 -0500

    puting test around packer install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 
 apt-get install -y lxc apt-cacher-ng unzip
 
-wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_amd64.zip
-unzip -d /usr/local/sbin packer_1.3.1_linux_amd64.zip
+if [ ! -e /usr/local/sbin/packer ]; then
+    wget https://releases.hashicorp.com/packer/1.3.1/packer_1.3.1_linux_amd64.zip
+    unzip -d /usr/local/sbin packer_1.3.1_linux_amd64.zip
+fi
 
 packer -version
+
